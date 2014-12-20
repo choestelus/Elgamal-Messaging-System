b0b98505e7bfdd573aa00b8d9f3fc18b192e1930
Choestelus <sprintf.null@gmail.com>
2014-12-20 21:48:48 +0700

    Add Lehmann Test

--- Elgamal2557.py
+++ Elgamal2557.py
@@ -1,4 +1,7 @@
 import unittest
+import os
+import binascii
+import random
 
 def testgen(total_num):
     return len(total_num) == len(set(total_num))
@@ -15,7 +18,6 @@ def generatorList(num):
     for i in range(1,num - 1):
         if isGenerator(i, num) == True:
             generator_list.append(i)
-        print i, isGenerator(i, num)
     return generator_list
 
 def egcd(a, b):
@@ -42,6 +44,24 @@ def exp_mod(x,n,m):
     elif n%2 != 0:
         return (x * exp_mod(x, (n-1)/2 ,m)**2) % m
 
+def getRandomBits(n):
+    #n = int(binascii.hexlify(os.urandom(n)), 16)
+    return random.SystemRandom().getrandbits(n)
+
+def getRandRange(a, b):
+    return random.SystemRandom().randint(a, b)
+
+def lehmannTest(test_number, test_count):
+    """docstring for lehmannTest"""
+    for i in range(test_count):
+        a = getRandRange(1, test_number - 1)
+        x = pow(a, (test_number - 1)//2, test_number)
+        if not (x == 1 or x == test_number - 1):
+            return False
+    else:
+        return True
+
+
 class TestMathProperties(unittest.TestCase):
     def setUp(self):
         pass
@@ -50,8 +70,20 @@ class TestMathProperties(unittest.TestCase):
         self.assertTrue(isGenerator(2,11))
         self.assertFalse(isGenerator(3,11))
 
+    def test_lehmannTest(self):
+        self.assertTrue(lehmannTest(2, 256))
+        self.assertTrue(lehmannTest(3, 256))
+        self.assertTrue(lehmannTest(5, 256))
+        self.assertTrue(lehmannTest(7, 256))
+        self.assertTrue(lehmannTest(11, 256))
+        self.assertTrue(lehmannTest(11, 256))
+        self.assertTrue(lehmannTest(1299541, 256))
+
+        
+        self.assertFalse(lehmannTest(488881, 256))
+
+
 if __name__ == '__main__':
+    #print "generator", random.SystemRandom().sample(generatorList(23), 1)[0]
     unittest.main()
-    print isGenerator(3,11)
-    print isGenerator(2,11)
 
