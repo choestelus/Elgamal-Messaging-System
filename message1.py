d0589b15f2d3477ba5dfce0638a83f0f09139ff1
Choestelus <sprintf.null@gmail.com>
2014-12-24 21:04:01 +0700

    Init file sending via tcp protocal

--- message1.py
+++ message1.py
@@ -2,7 +2,23 @@ import zmq
 import time
 import sys
 import threading
-import Elgamal2557
+import cPickle as pickle
+import zlib
+import json
+from Elgamal2557 import *
+
+def send_zipped_pickle(socket, obj, flags=0, protocol=-1):
+    """pickle an object, and zip the pickle before sending it"""
+    print "obj=", obj
+    p = pickle.dumps(obj, protocol)
+    #z = zlib.compress(p)
+    return socket.send(p, flags=flags)
+
+def recv_zipped_pickle(socket, flags=0, protocol=-1):
+    """inverse of send_zipped_pickle"""
+    z = socket.recv(flags)
+    p = zlib.uncompress(z)
+    return pickle.loads(p)
 
 def receiver_thread(arg1, stop_event):
     port = "5557"
@@ -27,19 +43,28 @@ if __name__ == "__main__":
     thread = threading.Thread(target = receiver_thread, args=(1, t_stop))
     thread.start()
 
-
     port = "5556"
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
     socket.connect("tcp://localhost:%s" % port)
 
+    key = gen_key(32)
+    file_send = BitStream(filename="README.md")
+    flag_sndfile = False
     while True:
         input = raw_input(">")
-
         if input == "stop":
             t_stop.set()
             print "exit program"
             sys.exit()
-            
-        socket.send(input)
+        elif input == "!file":
+            flag_sndfile = True
+            print "sending file"
+            socket.send("!file")
+            time.sleep(1)
+            send_zipped_pickle(socket, file_send)
+
+        if flag_sndfile == False:
+            socket.send(input)
+        flag_sndfile = False
         time.sleep(1)
--- message2.py
+++ message2.py
@@ -2,6 +2,22 @@ import zmq
 import time
 import sys
 import threading
+import cPickle as pickle
+import zlib
+from Elgamal2557 import *
+
+def send_zipped_pickle(socket, obj, flags=0, protocol=-1):
+    """pickle an object, and zip the pickle before sending it"""
+    p = pickle.dumps(obj, protocol)
+    z = zlib.compress(p)
+    return socket.send(z, flags=flags)
+
+def decomp_zipped_pickle(msg_recv):
+    """inverse of send_zipped_pickle"""
+    z = msg_recv
+    p = zlib.decompress(z)
+    return pickle.loads(p)
+
 
 def receiver_thread(arg1, stop_event):
     port = "5556"
@@ -11,15 +27,40 @@ def receiver_thread(arg1, stop_event):
 
     poller = zmq.Poller()
     poller.register(socket, zmq.POLLIN)
+    ffflag, fflag = False, False
 
     while(not stop_event.is_set()):
         socks = dict(poller.poll(1000))
         if socks:
             if socks.get(socket) == zmq.POLLIN:
                 msg = socket.recv(zmq.NOBLOCK)
-                print "receive: ", msg
-                sys.stdout.write(">")
-                sys.stdout.flush()
+
+                if msg == "!file":
+                    print "inside checksend"
+                    fflag = True
+                else:
+                    fflag = False
+
+                if ffflag == False:
+                    print "receive: ", msg
+                    sys.stdout.write(">")
+                    sys.stdout.flush()
+                    if fflag == True:
+                        ffflag = True
+                else:
+                    #print "raw message to loads:", msg
+                    #dsmsg = decomp_zipped_pickle(msg)
+                    dsmsg = pickle.loads(msg)
+                    #print dsmsg
+                    sys.stdout.write(">")
+                    sys.stdout.flush()
+                    frecv = BitStream(dsmsg)
+                    f = open("file.out", "wb")
+                    frecv.tofile(f)
+                    f.close()
+                    msg = "cleared"
+                    fflag = False
+                    ffflag = False
 
 if __name__ == "__main__":
     t_stop = threading.Event()
@@ -39,6 +80,5 @@ if __name__ == "__main__":
             t_stop.set()
             print "exit program"
             sys.exit()
-            
         socket.send(input)
         time.sleep(1)
