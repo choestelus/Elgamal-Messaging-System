96a1b76b4968ae6cd09f5d87753c7cd57feb6328
n1ghthawk <n.thanaphon.soo@gmail.com>
2014-12-24 21:07:40 +0700

    key exchange

--- message2.py
+++ message2.py
@@ -2,8 +2,12 @@ import zmq
 import time
 import sys
 import threading
+from Elgamal2557 import *
+import re
+import pickle
 
 def receiver_thread(arg1, stop_event):
+    global receiver_key 
     port = "5556"
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
@@ -17,28 +21,66 @@ def receiver_thread(arg1, stop_event):
         if socks:
             if socks.get(socket) == zmq.POLLIN:
                 msg = socket.recv(zmq.NOBLOCK)
-                print "receive: ", msg
+
+                if not receiver_key:
+                    receiver_key = pickle.loads(msg)
+                    print receiver_key, "receiver_key"
+                    continue
+
+                print "receive: ",
+                print decrypt_string(pickle.loads(msg), key)
                 sys.stdout.write(">")
                 sys.stdout.flush()
+
 
 if __name__ == "__main__":
     t_stop = threading.Event()
     thread = threading.Thread(target = receiver_thread, args=(1, t_stop))
     thread.start()
 
+    receiver_key = ()
 
     port = "5557"
     context = zmq.Context()
     socket = context.socket(zmq.PAIR)
+    socket.setsockopt(zmq.LINGER, 0)
     socket.connect("tcp://localhost:%s" % port)
+
+    #key generation
+    n, k = sys.argv[1:]
+    print n, k, "<- n k"
+    key = gen_key(10)
+    pub_key_str = pickle.dumps(key[0])
+    socket.send(pub_key_str)
 
     while True:
         input = raw_input(">")
 
-        if input == "stop":
+        try:
+            command = re.search('^\$.*\s', input).group(0)[:-1]
+            input = input.split(" ", 1)[1]
+        except AttributeError:
+            command = ""
+
+        if input == "$q":
             t_stop.set()
             print "exit program"
+            socket.close()
             sys.exit()
+            print "after exit"
+
+        if input == "":
+            continue 
+
+        if 'f' in command:
+            #sending file
+            pass
+
+        if 'e' in command:
+            print receiver_key , " <- receiver key"
+            ciphertext = encrypt_string(input, receiver_key)
+            print ciphertext, " <- cihpertext"
+            sending_str = pickle.dumps(ciphertext)
             
-        socket.send(input)
+        socket.send(sending_str)
         time.sleep(1)
